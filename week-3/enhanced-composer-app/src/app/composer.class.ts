1ba323f557830fc54ca8a69f234cfc598f29fad8
Cait Johnson <caitjohnson@wrightsgate.net>
2023-06-10 14:41:45 -0800

    Add files via upload

---
+++ week-3/enhanced-composer-app/src/app/composer.class.ts
@@ -0,0 +1,37 @@
+import { IComposer } from './composer.interface';
+export class Composer {
+    composers: Array<IComposer>;
+    
+    constructor() {
+        this.composers = [
+            {
+                composerId: 100, fullName: "John Williams", genre: "Film Composer"
+            },
+            {
+                composerId: 101, fullName: "Nobuo Uematsu", genre: "Video Game Composer"
+            },
+            {
+                composerId: 102, fullName: "Ludwig Beethoven", genre: "Classical Composer"
+            },
+            {
+                composerId: 103, fullName: "Claude Debussy", genre: "Classical Composer"
+            },
+            {
+                composerId: 104, fullName: "Martin O Donnell", genre: "Video Game Composer"
+            },
+        ]
+  }
+
+  getComposers() {
+    return this.composers;
+  }
+
+
+  getComposer(composerId: number) {
+    for (let composer of this.composers) {
+        if (composer.composerId === composerId) {
+            return composer;
+        }
+    }
+  }
+}
