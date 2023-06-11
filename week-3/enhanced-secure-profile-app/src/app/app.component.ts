59f60ef7225ab730c06fea45a54153d9795f7585
Cait Johnson <caitjohnson@wrightsgate.net>
2023-06-11 01:44:18 -0800

    Add files via upload

---
+++ week-3/enhanced-secure-profile-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  assignment: string = "Exercise 3.3 - Passing Data to Routes, Part 2";
+}
