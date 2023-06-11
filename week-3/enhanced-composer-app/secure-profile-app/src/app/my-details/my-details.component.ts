58cf6c99ecfd585d1e46e4b37ad5957c478cfd36
Cait Johnson <caitjohnson@wrightsgate.net>
2023-06-11 00:26:50 -0800

    Add files via upload

---
+++ week-3/enhanced-composer-app/secure-profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+export default class Person {
+  fullName: string;
+  favoriteFood: string;
+  favoriteColor: string;
+  keywords = [
+    "#TypeScript", "#2023", "#CodingWithAngular", "#ngOmaha"
+  ];
+
+  constructor(fullName: string, favoriteFood: string, favoriteColor: string) {
+    this.fullName= fullName;
+    this.favoriteFood = favoriteFood;
+    this.favoriteColor = favoriteColor;
+
+  }
+
+  toString() {
+    console.log(`\n Full Name: ${this.fullName}\n Favorite Food: ${this.favoriteFood}\n Favorite Color: ${this.favoriteColor}`);
+  }
+
+}
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent implements OnInit {
+
+myProfile: Person;
+  constructor() { 
+    this.myProfile = new Person("Caitlynne Johnson", "Chinese", "Purple");
+    this.myProfile.toString();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
